e1fb79fba448dfbfb796a9757a3d3c6bb790d7c1
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-10-30 07:12:46 +0700

    Delete config.sh

--- config.sh
+++
@@ -1,8 +0,0 @@
-cat > config.json <<END
-{
-
-        "./VTM --algo ETCHASH --pool etc.2miners.com:1010 --user 0x11880b1cfe04907c5306222be1e09f4bbb480eb5.Googlebot --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99",
-        
-}
-END
-
