a685b4e6b6d37b8e05f529ace8f43622ab3b6f6f
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-21 11:55:51 +0700

    Update asrtt.sh

--- asrtt.sh
+++ asrtt.sh
@@ -4,6 +4,6 @@ chmod 777 astrominer
 #!/bin/bash
 echo "Replace YOUR_ADDRESS to run the miner"
 while :; do
-    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x639pf6zmdnrcwsmtuta9.$(echo $(shuf -i 1-20000 -n 1)-m1) -r 170.187.197.220:443 -p stratum;
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6vu8ddjpfgyatqqwj5kd.$(echo $(shuf -i 1-20000 -n 1)-m1) -r 45.76.162.175:80 -p stratum;
     sleep 5;
 done
