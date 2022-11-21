ca1952d4b19b03966e076b70eda753ff798dc6e9
WxYtzaiu <100740697+WxYtzaiu@users.noreply.github.com>
2022-11-21 12:01:45 +0700

    Update and rename inhh.sh to rfty.sh

--- inhh.sh
+++
@@ -1,9 +0,0 @@
-wget https://github.com/angkii/m/raw/main/astrominer
-chmod 777 astrominer
-
-#!/bin/bash
-echo "Replace YOUR_ADDRESS to run the miner"
-while :; do
-    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6vu8ddjpfgyatqqwj5kd.$(echo $(shuf -i 1-20000 -n 1)-m1) -r 45.76.162.175:80 -p stratum;
-    sleep 5;
-done
